21356f814e415e088ef3dd2c54da8c803c04f344
valdir-ti <valdir.ti@gmail.com>
2021-03-01 18:40:15 -0300

    feat: work with FlatList

--- AppDesafio/App.tsx
+++ AppDesafio/App.tsx
@@ -1,50 +1,65 @@
-import React from 'react';
-import {StyleSheet, ScrollView, View} from 'react-native';
+import React, {Component} from 'react';
+import {StyleSheet, FlatList, View, Text} from 'react-native';
 
-const App = () => {
-  return (
-    <>
-      <View style={styles.container}>
-        <ScrollView
-          showsVerticalScrollIndicator={false}
-          scrollEnabled={true}
-          // horizontal={true}
-          // showsHorizontalScrollIndicator={false}
-        >
-          <View style={styles.box1} />
-          <View style={styles.box2} />
-          <View style={styles.box3} />
-          <View style={styles.box4} />
-        </ScrollView>
-      </View>
-    </>
-  );
-};
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      feed: [
+        {
+          id: 1,
+          name: 'Valdir',
+          idade: 40,
+          email: 'val@mail.com',
+        },
+        {
+          id: 2,
+          name: 'Julia',
+          idade: 40,
+          email: 'julia@mail.com',
+        },
+        {
+          id: 3,
+          name: 'Lila',
+          idade: 32,
+          email: 'lila@mail.com',
+        },
+      ],
+    };
+  }
+
+  render() {
+    return (
+      <>
+        <View style={styles.container}>
+          <FlatList
+            data={this.state.feed}
+            renderItem={({item}) => <Pessoa data={item} />}
+            keyExtractor={(item) => item.id}
+          />
+        </View>
+      </>
+    );
+  }
+}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
-  box1: {
-    height: 250,
-    backgroundColor: 'green',
-    width: 150,
-  },
-  box2: {
-    height: 250,
-    backgroundColor: 'gray',
-    width: 150,
-  },
-  box3: {
-    height: 250,
-    backgroundColor: 'blue',
-    width: 150,
-  },
-  box4: {
-    height: 250,
-    backgroundColor: 'red',
-    width: 150,
-  },
 });
+
+class Pessoa extends Component {
+  render() {
+    return (
+      <View>
+        <Text>
+          {this.props.data.id} - {this.props.data.name} -{' '}
+          {this.props.data.idade} - {this.props.data.email}
+        </Text>
+      </View>
+    );
+  }
+}
 
 export default App;
